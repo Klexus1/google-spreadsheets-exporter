b15a280b25708e6cd70efddd72357158a2ee1b90
David Louda <david.louda@unicorn.com>
2023-07-24 18:48:25 +0200

    test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name='google_spreadsheets_exporter',
-    version='1.0.1',
+    version='1.0.2',
     packages=['google_spreadsheets_exporter'],
     install_requires=[
         'google-api-python-client',
